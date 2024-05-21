653733949cf1b4a8e283c2315f13cdf736e5db66
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-21 15:06:51 +0900

    Update: 공격 딜레이 기능 추가

--- Assets/Scripts/Controller/IAEController.cs
+++ Assets/Scripts/Controller/IAEController.cs
@@ -10,9 +10,31 @@ public class IAEController : MonoBehaviour
     public event Action<AttackSO> OnAttackEvent;
     protected CharacterStatHandler stats { get; private set; }
 
+    protected bool IsAttacking {  get; set; }
+
+    private float timeSinceLastAttack = float.MaxValue;
+
     protected virtual void Awake()
     {
         stats = GetComponent<CharacterStatHandler>();
+    }
+
+    private void Update()
+    {
+        HandleAttackDelay();
+    }
+
+    private void HandleAttackDelay()
+    {
+        if (timeSinceLastAttack < stats.CurrentStat.attackSO.delay)
+        {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+        else if ( IsAttacking && timeSinceLastAttack >= stats.CurrentStat.attackSO.delay)
+        {
+            timeSinceLastAttack = 0.0f;
+            CallAttackEvent(stats.CurrentStat.attackSO);
+        }
     }
 
     public void CallMoveEvent(Vector2 direction)
