ad64b3f47a984b719c57f1b2ccd03fa8737b9e1e
youminkim <kym1787@naver.com>
2024-05-21 12:02:52 +0900

    Add : 버튼 기능구현

--- Assets/Scripts/UI/IntroUI.cs
+++ Assets/Scripts/UI/IntroUI.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Windows;
+using UnityEngine.SceneManagement;
 
 public class IntroUI : MonoBehaviour
 {
+    
+    public TMP_InputField inputField;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+   
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickStartBtn()
     {
-        
+
+        if (inputField.text.Length == 4 && inputField.text[0] == 'i' || inputField.text[0] == 'I')
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (inputField.text[0] == 'e' || inputField.text[0] == 'E')
+        {
+            return;
+        }
+        else return;
     }
 }
