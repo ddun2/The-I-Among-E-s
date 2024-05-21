de8c5af31aee15a52ca0e4ed9137a3bb5c928b3d
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-21 13:47:22 +0900

    Add: IAEAimRotation.cs 파일 추가, Feature: 회전 기능 구현

---
+++ Assets/Scripts/Behaviors/IAEAimRotaion.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAEAimRotaion : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer armRenderer;
+    [SerializeField] private Transform armPivot;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private IAEController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<IAEController>();
+    }
+
+    void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direction)
+    {
+        RotateAim(direction);
+    }
+
+    private void RotateAim(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+        armRenderer.flipY = characterRenderer.flipX;
+        armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
